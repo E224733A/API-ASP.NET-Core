b5ea3cbf693d220cea56bba94c19dff720959027
Noé BRACQ <noetude44@gmail.com>
2026-04-29 07:48:26 +0200

    Configure connexions SQL et endpoints de diagnostic

--- Controllers/DebugSqlController.cs
+++ Controllers/DebugSqlController.cs
@@ -30,79 +30,21 @@ public class DebugSqlController : ControllerBase
         return Ok(tables);
     }
 
-    [HttpGet("tables-mobile/details")]
-    public async Task<IActionResult> GetTablesMobileDetails()
-    {
-        using var connection = _connectionFactory.CreateMobileConnection();
-
-        var tables = await connection.QueryAsync("""
-            SELECT 
-                t.name AS TableName,
-                SUM(p.rows) AS NombreLignes
-            FROM sys.tables t
-            INNER JOIN sys.partitions p ON t.object_id = p.object_id
-            WHERE t.name LIKE 'Mobile_%'
-              AND p.index_id IN (0, 1)
-            GROUP BY t.name
-            ORDER BY t.name;
-            """);
-
-        return Ok(tables);
-    }
-
-    [HttpGet("contraintes-mobile")]
-    public async Task<IActionResult> GetContraintesMobile()
-    {
-        using var connection = _connectionFactory.CreateMobileConnection();
-
-        var contraintes = await connection.QueryAsync("""
-            SELECT 
-                t.name AS TableName,
-                c.name AS ConstraintName,
-                c.type_desc AS ConstraintType
-            FROM sys.objects c
-            INNER JOIN sys.tables t ON c.parent_object_id = t.object_id
-            WHERE t.name LIKE 'Mobile_%'
-              AND c.type IN ('C', 'F', 'PK', 'UQ')
-            ORDER BY t.name, c.type_desc, c.name;
-            """);
-
-        return Ok(contraintes);
-    }
-
-    [HttpGet("index-mobile")]
-    public async Task<IActionResult> GetIndexMobile()
-    {
-        using var connection = _connectionFactory.CreateMobileConnection();
-
-        var index = await connection.QueryAsync("""
-            SELECT 
-                t.name AS TableName,
-                i.name AS IndexName,
-                i.is_unique AS EstUnique,
-                i.filter_definition AS Filtre
-            FROM sys.indexes i
-            INNER JOIN sys.tables t ON i.object_id = t.object_id
-            WHERE t.name LIKE 'Mobile_%'
-              AND i.name IS NOT NULL
-            ORDER BY t.name, i.name;
-            """);
-
-        return Ok(index);
-    }
-
     [HttpGet("vues-abssolute")]
-    public async Task<IActionResult> GetVuesAbssolute()
+    public IActionResult GetVuesAbssoluteConnues()
     {
-        using var connection = _connectionFactory.CreateAbssoluteConnection();
-
-        var vues = await connection.QueryAsync("""
-            SELECT 
-                TABLE_SCHEMA AS SchemaName,
-                TABLE_NAME AS ViewName
-            FROM INFORMATION_SCHEMA.VIEWS
-            ORDER BY TABLE_NAME;
-            """);
+        var vues = new[]
+        {
+            "v_tournee",
+            "v_fermeture",
+            "v_chauffeurs",
+            "v_clients",
+            "v_pdl_jour",
+            "v_liste_article",
+            "v_liste_produit_abssolute",
+            "v_jour_client",
+            "v_route_number"
+        };
 
         return Ok(vues);
     }
@@ -131,5 +73,96 @@ public class DebugSqlController : ControllerBase
             """);
 
         return Ok(tournees);
+    }
+
+    [HttpGet("clients")]
+    public async Task<IActionResult> GetClients()
+    {
+        using var connection = _connectionFactory.CreateAbssoluteConnection();
+
+        var clients = await connection.QueryAsync("""
+            SELECT TOP 10 *
+            FROM v_clients;
+            """);
+
+        return Ok(clients);
+    }
+
+    [HttpGet("pdl-jour")]
+    public async Task<IActionResult> GetPdlJour()
+    {
+        using var connection = _connectionFactory.CreateAbssoluteConnection();
+
+        var pdlJour = await connection.QueryAsync("""
+            SELECT TOP 10 *
+            FROM v_pdl_jour;
+            """);
+
+        return Ok(pdlJour);
+    }
+
+    [HttpGet("fermetures")]
+    public async Task<IActionResult> GetFermetures()
+    {
+        using var connection = _connectionFactory.CreateAbssoluteConnection();
+
+        var fermetures = await connection.QueryAsync("""
+            SELECT TOP 10 *
+            FROM v_fermeture;
+            """);
+
+        return Ok(fermetures);
+    }
+
+    [HttpGet("jour-client")]
+    public async Task<IActionResult> GetJourClient()
+    {
+        using var connection = _connectionFactory.CreateAbssoluteConnection();
+
+        var joursClient = await connection.QueryAsync("""
+            SELECT TOP 10 *
+            FROM v_jour_client;
+            """);
+
+        return Ok(joursClient);
+    }
+
+    [HttpGet("route-number")]
+    public async Task<IActionResult> GetRouteNumber()
+    {
+        using var connection = _connectionFactory.CreateAbssoluteConnection();
+
+        var routes = await connection.QueryAsync("""
+            SELECT TOP 10 *
+            FROM v_route_number;
+            """);
+
+        return Ok(routes);
+    }
+
+    [HttpGet("articles")]
+    public async Task<IActionResult> GetArticles()
+    {
+        using var connection = _connectionFactory.CreateAbssoluteConnection();
+
+        var articles = await connection.QueryAsync("""
+            SELECT TOP 10 *
+            FROM v_liste_article;
+            """);
+
+        return Ok(articles);
+    }
+
+    [HttpGet("produits-abssolute")]
+    public async Task<IActionResult> GetProduitsAbssolute()
+    {
+        using var connection = _connectionFactory.CreateAbssoluteConnection();
+
+        var produits = await connection.QueryAsync("""
+            SELECT TOP 10 *
+            FROM v_liste_produit_abssolute;
+            """);
+
+        return Ok(produits);
     }
 }
